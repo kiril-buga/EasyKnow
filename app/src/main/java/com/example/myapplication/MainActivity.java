83ae28693ba1c360258007cb3a035a5b3fc5526c
Kiril <kirilbuga@gmail.com>
2021-05-13 14:36:36 +0200

    Notifications base

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,75 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btShow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+
+        //Assign variable
+        btShow = findViewById(R.id.bt_show);
+
+        //Convert image type to bitmap
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.picture);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel1")
+                .setSmallIcon(R.drawable.ic_message)
+                .setContentTitle("Notification")
+                .setContentText("This is notification")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setLargeIcon(icon);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        btShow.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            public void onClick(View v) {
+
+                notificationManager.notify(100, builder.build());
+
+
+            }
+        });
 
     }
+
+    private void createNotificationChannel(){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "Channel";
+            String description = "Channel for notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("channel1", name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+
 
 }
