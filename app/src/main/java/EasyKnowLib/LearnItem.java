504118861d5dddd22f5c9ffec0ecab0996b680f0
leandro-hoenen <leandro.hoenen@gmail.com>
2021-05-15 18:16:04 +0200

    LearnItem Class

---
+++ app/src/main/java/EasyKnowLib/LearnItem.java
@@ -0,0 +1,37 @@
+package EasyKnowLib;
+
+public class LearnItem {
+    private String title;
+    private String meaning;
+    private LearnItemStatus learnStatus;
+
+    public LearnItem(String title, String meaning) {
+        this.title = title;
+        this.meaning = meaning;
+        this.learnStatus = new LearnItemStatus();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    public LearnItemStatus getLearnStatus() {
+        return learnStatus;
+    }
+
+    public void setLearnStatus(LearnItemStatus learnStatus) {
+        this.learnStatus = learnStatus;
+    }
+}
