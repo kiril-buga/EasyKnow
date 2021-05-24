eabd504f3344500bc733819f060825adc9256a9d
leandro-hoenen <leandro.hoenen@gmail.com>
2021-05-24 18:24:45 +0200

    Implemented switches

--- app/src/main/java/com/example/myapplication/NotificationManagerActivity.java
+++ app/src/main/java/com/example/myapplication/NotificationManagerActivity.java
@@ -11,33 +11,62 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
+import android.widget.Switch;
+
+import EasyKnowLib.Week;
 
 public class NotificationManagerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
+    // Controls
     private Spinner spinner;
+    private Switch swMonday;
+    private Switch swTuesday;
+    private Switch swWednesday;
+    private Switch swThursday;
+    private Switch swFriday;
+    private Switch swSaturday;
+    private Switch swSunday;
+
     private int currentNotificationNumber;
+    private Week week = new Week();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_manager);
 
+        // Spinner
         Spinner spinner = (Spinner) findViewById(R.id.spinnerNotificationAmount);
-// Create an ArrayAdapter using the string array and a default spinner layout
+        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.numberOfNotifications, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-
         spinner.setOnItemSelectedListener(this);
 
+        // Toolbar
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbarNotificationManager);
         myToolbar.setTitle("Notification Settings");
         setSupportActionBar(myToolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Switches
+        swMonday = findViewById(R.id.switchMonday);
+        swMonday.setOnCheckedChangeListener(this::setDay);
+        swTuesday = findViewById(R.id.switchTuesday);
+        swWednesday = findViewById(R.id.switchWednesday);
+        swThursday = findViewById(R.id.switchThursday);
+        swFriday = findViewById(R.id.switchFriday);
+        swSaturday = findViewById(R.id.switchSaturday);
+        swSunday = findViewById(R.id.switchSunday);
+    }
+
+    private void setDay(CompoundButton compoundButton, boolean b) {
+
     }
 
 
